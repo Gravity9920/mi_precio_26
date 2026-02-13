edde3c513ff49bd029704cfcd9c603ac6c956af1
Gravity9920 <guti9920@gmail.com>
2026-02-13 18:16:27 -0500

    precios_achahui_0.10

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,7 @@ app.use(express.json());
 
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 
